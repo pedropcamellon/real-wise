a95728c22b5090f30bd45cc5a13cd855b95e3b79
Pedro P. Camellon <pedropcamellon@gmail.com>
2024-11-18 22:03:20 -0500

    refactor: simplify admin configuration
    - Remove Unfold ModelAdmin inheritance
    - Remove unused Group import
    - Combine role and permission sections
    - Simplify UserAdmin configuration
    - Remove redundant fields

--- backend/src/backend/admin.py
+++ backend/src/backend/admin.py
@@ -1,22 +1,54 @@
 from django.contrib import admin
-from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import Group
-from unfold.admin import ModelAdmin
-from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
-from .models import User
+from .models import User, UserRole
 
-admin.site.unregister(Group)
+
+@admin.register(UserRole)
+class UserRoleAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "created_at", "modified_at")
+    search_fields = ("name", "description")
+    ordering = ("name",)
+    readonly_fields = ("created_at", "modified_at")
 
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin, ModelAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
-    change_password_form = AdminPasswordChangeForm
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = ("is_staff", "is_active", "roles")
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("username",)
 
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Roles and Permissions", {"fields": ("roles", "is_staff", "is_active")}),
+        (
+            "Important dates",
+            {"fields": ("last_login", "date_joined", "created_at", "modified_at")},
+        ),
+    )
 
-@admin.register(Group)
-class GroupAdmin(BaseGroupAdmin, ModelAdmin):
-    pass
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "roles",
+                ),
+            },
+        ),
+    )
+    readonly_fields = ("created_at", "modified_at", "last_login", "date_joined")
